4d6f91e2266d1b9d1b0fad428609992037902b08
DevHuba <devhuba@gmail.com>
2022-08-09 13:28:36 +0300

    add background + rest timer

--- app/src/main/java/eu/devhuba/a7_minutes_workout/ExerciseActivity.kt
+++ app/src/main/java/eu/devhuba/a7_minutes_workout/ExerciseActivity.kt
@@ -2,6 +2,9 @@ package eu.devhuba.a7_minutes_workout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.WindowManager
+import android.widget.Toast
 import eu.devhuba.a7_minutes_workout.databinding.ActivityExerciseBinding
 
 
@@ -9,22 +12,70 @@ class ExerciseActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityExerciseBinding
 
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
+    private val restStartTime = 10
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.tbExercise)
-        
+
+        //Hide toolbar title
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        //Start preparation timer
+        setUpRestView()
+
         if (supportActionBar != null) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        } 
+        }
 
         //Back button logic
         binding.tbExercise.setNavigationOnClickListener {
             onBackPressed()
         }
 
+
     }
+    
+    private fun setUpRestView() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar() {
+        binding.progressBar.progress = restProgress
+
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding.progressBar.progress = restStartTime - restProgress
+                binding.tvTimer.text = (restStartTime - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "Rest is done need to start with exercise", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }.start()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+    }
+
 }
